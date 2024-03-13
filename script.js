9809d45631e91516899591e2ca19034b88534f6c
looyeh <asadi.loyeh@gmail.com>
2024-03-13 12:49:37 +0330

    Adding check function

--- script.js
+++ script.js
@@ -16,6 +16,25 @@ const richest = [
   "Larry Page",
   "Sergey Brin",
 ];
+const richest1 = [
+  "Elon Musk",
+  "Jeff Bezos",
+  "Bernard Arnault",
+  "Mark Zuckerberg",
+  "Bill Gates",
+  "Steve Ballmer",
+  "Warren Buffett",
+  "Larry Ellison",
+  "Larry Page",
+  "Sergey Brin",
+];
+
+const persons = document.getElementsByClassName("person");
+
+const col2 = document.getElementsByClassName("col2");
+
+const checkBtn=document.getElementById('check');
+
 function shuffle(array) {
   let currentIndex = array.length,
     randomIndex;
@@ -36,16 +55,26 @@ function shuffle(array) {
   return array;
 }
 
+function check(){
+  for(i=0;i<10;i++){
+    if(col2[i].innerText==richest[i]){
+      col2[i].classList.add("green")
+    }else{
+      col2[i].classList.add("red")
+    };
+  };
+}
+
 window.addEventListener("DOMContentLoaded", () => {
-  const persons = document.getElementsByClassName("person");
-  const col2 = document.getElementsByClassName("col2");
-  const arr = shuffle( richest );
+  
+  const arr = shuffle( richest1 );
   console.log(arr); 
   
   for (i = 0; i < persons.length; i++) {
     persons[i].addEventListener("dragstart", dragstartHandler);
     col2[i].innerText = arr[i];
   }
+  //checkBtn.addEventListener( "click" ,check);
 });
 // 1. Elon Musk 2. Jeff Bezos 3. Bernard Arnault 4. Mark Zuckerberg
 // 5. Bill Gates 6. Steve Ballmer 7. Warren Buffett 8. Larry Ellison
